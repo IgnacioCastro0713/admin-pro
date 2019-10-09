085dff4c31b17e57eead01dd49a9322abf81cb8e
ignacio <ignaciomenchaca666@gmail.com>
2019-10-09 14:04:49 -0500

    template integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DasboardComponent } from './views/dasboard/dasboard.component';
+import { Charts1Component } from './views/charts1/charts1.component';
+import { PagenotfoundComponent } from './views/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './components/shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DasboardComponent,
+    Charts1Component,
+    PagenotfoundComponent,
+    LoginComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/dasboard/dasboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dasboard',
+  templateUrl: './dasboard.component.html',
+})
+export class DasboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
